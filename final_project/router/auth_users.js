71d04428bff4dd53a99222c880ea3b84a31c2ddc
Jack Huynh <deadpenpen@gmail.com>
2026-03-02 15:25:15 -0600

    task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -83,8 +83,34 @@ regd_users.post("/register", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented q"});
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  // Check login
+  if (!req.session.authorization) {
+    return res.status(401).json({ message: "User not logged in" });
+  }
+
+  const username = req.session.authorization.username;
+
+  // Check if review text exists
+  if (!review) {
+    return res.status(400).json({ message: "Review text is required" });
+  }
+
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  // Add or update review
+  book.reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review added/updated successfully",
+    reviews: book.reviews
+  });
 });
 
 module.exports.authenticated = regd_users;
