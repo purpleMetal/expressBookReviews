582b91c6aa5f20f8409b32eb924b7d74f782dde4
Jack Huynh <deadpenpen@gmail.com>
2026-03-02 15:53:07 -0600

    task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,11 +40,28 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
 
-  res.send(books[isbn]);
- });
+    const response = await axios.get("http://localhost:3000/books");
+    const books = response.data;
+
+    const book = books[isbn];
+
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error fetching book",
+      error: error.message
+    });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
